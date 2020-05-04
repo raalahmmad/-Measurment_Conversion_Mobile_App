88079d1c0f8272e6785d28ad0c1a007b328db048
Raed Alahmad <raalahmad@hotmail.com>
2020-05-04 17:03:23 -0400

    dialog

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+main(List<String> args) {
+  return runApp(MyApp2());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Hello World Travel Title",
+        home: Scaffold(
+            appBar: AppBar(title: Text("Hello World Travel App")),
+            body: Center(child: Text('Hello World Travel'))));
+  }
+}
+
+class MyApp2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var ctx = context;
+    return MaterialApp(
+        title: "My App",
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Raed App'),
+              backgroundColor: Colors.deepPurple,
+            ),
+            body: Builder(
+                builder: (context) => Center(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Here we go ...',
+                          style: TextStyle(
+                              fontSize: 26,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue[800]),
+                        ),
+                        Text('second line'),
+                        Image.network(
+                          'https://images.freeimages.com/images/large-previews/eaa/the-beach-1464354.jpg',
+                          height: 350,
+                        ),
+                        RaisedButton(
+                          child: Text('Contact us'),
+                          onPressed: () => contactUs(context),
+                        )
+                      ],
+                    )))));
+  }
+
+  void contactUs(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          width: 100,
+          height: 100,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text('App title'),
+            ),
+            body: Text('Second layer'),
+          ),
+        );
+      },
+    );
+  }
+}
